58860ae18a18611e9778c235379583202cc84fa3
arpit <arpit.tarang@gmail.com>
2018-06-13 18:51:00 +0530

    added aws websockets

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,23 @@ import "./App.css";
 import Gauge from "./Gauge";
 import moment from "moment";
 import { ScaleLoader } from "react-spinners";
-import mqtt from "mqtt";
+import { cert, priv, rt } from "./certs.json";
+import AWS from "aws-sdk";
+import AWSIoTData from "aws-iot-device-sdk";
+import AWSConfiguration from "./aws-configuration";
 
+AWS.config.region = AWSConfiguration.region;
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials({
+  IdentityPoolId: AWSConfiguration.poolId
+});
+
+//
+// Create a client id to use when connecting to AWS IoT.
+//
+var clientId = "mqtt-explorer-" + Math.floor(Math.random() * 100000 + 1);
+
+const baseUrl = AWSConfiguration.lambda;
 const baseStyles = {
   padding: "0.5rem",
   textAlign: "center"
@@ -19,33 +34,96 @@ class App extends Component {
 
   onMotorClicked = () => {
     const { client, latestPoint: { motor } } = this.state;
-    client.publish("arpit/waterpump/motor", motor ? "0" : "1");
+    client.publish("motor", motor ? "0" : "1");
     this.setState({ loading: true });
   };
 
   componentDidMount() {
     // fetch first point from server only
-    fetch(`/api/point/latest`)
+    fetch(`${baseUrl}/getlatestdatapoint`)
       .then(res => res.json())
       .then(latestPoint => ({
-        ...latestPoint,
-        timestamp: moment(latestPoint.timestamp).unix()
+        ...latestPoint
       }))
       .then(latestPoint => this.setState({ latestPoint }));
-    var client = mqtt.connect(
-      "wss://test.mosquitto.org:8081/" /*, {
-      username: "qfpvofcn",
-      password: "FdC_CFb_idiV"
-    }*/
-    );
+
+    var client = AWSIoTData.device({
+      //
+      // Set the AWS region we will operate in.
+      //
+      region: AWS.config.region,
+      //
+      ////Set the AWS IoT Host Endpoint
+      host: AWSConfiguration.host,
+      //
+      // Use the clientId created earlier.
+      //
+      clientId: clientId,
+      //
+      // Connect via secure WebSocket
+      //
+      protocol: "wss",
+      //
+      // Set the maximum reconnect time to 8 seconds; this is a browser application
+      // so we don't want to leave the user waiting too long for reconnection after
+      // re-connecting to the network/re-opening their laptop/etc...
+      //
+      maximumReconnectTimeMs: 8000,
+      //
+      // Enable console debugging information (optional)
+      //
+      debug: true,
+      //
+      // IMPORTANT: the AWS access key ID, secret key, and sesion token must be
+      // initialized with empty strings.
+      //
+      accessKeyId: "",
+      secretKey: "",
+      sessionToken: ""
+    });
+
+    //
+    // Attempt to authenticate to the Cognito Identity Pool.  Note that this
+    // example only supports use of a pool which allows unauthenticated
+    // identities.
+    //
+    var cognitoIdentity = new AWS.CognitoIdentity();
+    AWS.config.credentials.get(function(err, data) {
+      if (!err) {
+        console.log("retrieved identity: " + AWS.config.credentials.identityId);
+        var params = {
+          IdentityId: AWS.config.credentials.identityId
+        };
+        cognitoIdentity.getCredentialsForIdentity(params, function(err, data) {
+          if (!err) {
+            //
+            // Update our latest AWS credentials; the MQTT client will use these
+            // during its next reconnect attempt.
+            //
+            client.updateWebSocketCredentials(
+              data.Credentials.AccessKeyId,
+              data.Credentials.SecretKey,
+              data.Credentials.SessionToken
+            );
+          } else {
+            console.log("error retrieving credentials: " + err);
+            alert("error retrieving credentials: " + err);
+          }
+        });
+      } else {
+        console.log("error retrieving identity:" + err);
+        alert("error retrieving identity: " + err);
+      }
+    });
+
     client.on("connect", function() {
       console.log("connected");
-      client.subscribe("arpit/waterpump/status");
+      client.subscribe("status");
     });
 
     client.on("message", (topic, message) => {
       // message is Buffer
-      if (topic == "arpit/waterpump/status") {
+      if (topic == "status") {
         var latestPoint = JSON.parse(message.toString());
         this.setState({ latestPoint, loading: false });
       }
---
+++ client/src/aws-configuration.js
@@ -0,0 +1,6 @@
+export default {
+  poolId: "us-east-1:380a043a-783f-4679-8d11-57e90cac6958", // 'YourCognitoIdentityPoolId'
+  host: "a3dnhghs2v16ap.iot.us-east-1.amazonaws.com", // 'YourAwsIoTEndpoint', e.g. 'prefix.iot.us-east-1.amazonaws.com'
+  region: "us-east-1", // 'YourAwsRegion', e.g. 'us-east-1'
+  lambda: "https://fzy9csh2xf.execute-api.us-east-1.amazonaws.com/prod1" // lambda base url
+};
